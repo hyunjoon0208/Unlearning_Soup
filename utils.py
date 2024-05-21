6587c28aa05f687f67186628719fde1715ebf0c9
hyunjoon0208 <juatis96@kookmin.ac.kr>
2024-05-22 01:24:25 +0900

    fix utils:add accuracy

--- utils.py
+++ utils.py
@@ -15,140 +15,153 @@ from models import *
 
 
 __all__ = [
-    "setup_model_dataset",
-    "save_checkpoint",
-    "load_checkpoint",
-    "AverageMeter",
-    "setup_seed",
-    "warmup_lr"
+	"setup_model_dataset",
+	"save_checkpoint",
+	"load_checkpoint",
+	"AverageMeter",
+	"setup_seed",
+	"warmup_lr",
+	"accuracy",
 ]
 
-
+def accuracy(output, target, topk=(1,)):
+	maxk = max(topk)
+	batch_size = target.size(0)
+	
+	_, pred = output.topk(maxk, 1, True, True)
+	pred = pred.t()
+	correct = pred.eq(target.view(1, -1).expand_as(pred))
+	
+	res = []
+	for k in topk:
+		correct_k = correct[:k].view(-1).float().sum(0)
+		res.append(correct_k.mul_(100.0 / batch_size))
+	return res
 
 def setup_model_dataset(args):
-    if args.dataset == "cifar10":
-        num_classes = 10
-        normalize = transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010])
-        train_loader, val_loader, test_loader, retain_train_loader, retain_test_loader, forget_loader = cifar10_dataloaders(batch_size=args.batch_size, num_workers=args.num_workers)
-        marked_loader,_, test_loader, _, _, _= cifar10_dataloaders(
-            batch_size=args.batch_size, 
-            data_dir=args.data,
-            num_workers=args.num_workers,
-            class_to_replace=args.class_to_replace,
-            num_indexes_to_replace=args.num_indexes_to_replace,
-            indexes_to_replace=args.indexes_to_replace,
-            seed=args.seed,
-            only_mark=True,
-            shuffle=True,
-            no_aug=args.no_aug,
-        )
+	if args.dataset == "cifar10":
+		num_classes = 10
+		normalize = transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010])
+		train_loader, val_loader, test_loader, retain_train_loader, retain_test_loader, forget_loader = cifar10_dataloaders(batch_size=args.batch_size, num_workers=args.num_workers)
+		marked_loader,_, test_loader, _, _, _= cifar10_dataloaders(
+			batch_size=args.batch_size, 
+			data_dir=args.data,
+			num_workers=args.num_workers,
+			class_to_replace=args.class_to_replace,
+			num_indexes_to_replace=args.num_indexes_to_replace,
+			indexes_to_replace=args.indexes_to_replace,
+			seed=args.seed,
+			only_mark=True,
+			shuffle=True,
+			no_aug=args.no_aug,
+		)
 
-        if args.train_seed is None:
-            args.train_seed = args.seed
-        setup_seed(args.train_seed)
+		if args.train_seed is None:
+			args.train_seed = args.seed
+		setup_seed(args.train_seed)
 
-        model = model_dict[args.arch](num_classes=num_classes)
-        
-        setup_seed(args.train_seed)
-        model.normalize = normalize
-        print(model)
-        return model, train_loader, val_loader, test_loader, marked_loader
-    
-    elif args.dataset == "cifar100":
-        num_classes = 100
-        normalize = transforms.Normalize(mean=[0.5071, 0.4867, 0.4408], std=[0.2675, 0.2565, 0.2761])
-        train_loader, val_loader, test_loader, retain_train_loader, retain_test_loader, forget_loader = cifar100_dataloaders(batch_size=args.batch_size, num_workers=args.num_workers)
-        marked_loader,_, test_loader, _, _, _= cifar100_dataloaders(
-            batch_size=args.batch_size, 
-            data_dir=args.data,
-            num_workers=args.num_workers,
-            class_to_replace=args.class_to_replace,
-            num_indexes_to_replace=args.num_indexes_to_replace,
-            indexes_to_replace=args.indexes_to_replace,
-            seed=args.seed,
-            only_mark=True,
-            shuffle=True,
-            no_aug=args.no_aug,
-        )
+		model = model_dict[args.arch](num_classes=num_classes)
+		
+		setup_seed(args.train_seed)
+		model.normalize = normalize
+		print(model)
+		return model, train_loader, val_loader, test_loader, marked_loader
+	
+	elif args.dataset == "cifar100":
+		num_classes = 100
+		normalize = transforms.Normalize(mean=[0.5071, 0.4867, 0.4408], std=[0.2675, 0.2565, 0.2761])
+		train_loader, val_loader, test_loader, retain_train_loader, retain_test_loader, forget_loader = cifar100_dataloaders(batch_size=args.batch_size, num_workers=args.num_workers)
+		marked_loader,_, test_loader, _, _, _= cifar100_dataloaders(
+			batch_size=args.batch_size, 
+			data_dir=args.data,
+			num_workers=args.num_workers,
+			class_to_replace=args.class_to_replace,
+			num_indexes_to_replace=args.num_indexes_to_replace,
+			indexes_to_replace=args.indexes_to_replace,
+			seed=args.seed,
+			only_mark=True,
+			shuffle=True,
+			no_aug=args.no_aug,
+		)
 
-        if args.train_seed is None:
-            args.train_seed = args.seed
-        setup_seed(args.train_seed)
+		if args.train_seed is None:
+			args.train_seed = args.seed
+		setup_seed(args.train_seed)
 
-        model = model_dict[args.arch](num_classes=num_classes)
-        
-        setup_seed(args.train_seed)
-        model.normalize = normalize
-        print(model)
-        return model, train_loader, val_loader, test_loader, marked_loader
+		model = model_dict[args.arch](num_classes=num_classes)
+		
+		setup_seed(args.train_seed)
+		model.normalize = normalize
+		print(model)
+		return model, train_loader, val_loader, test_loader, marked_loader
 
 def save_checkpoint(state, save_path,name, is_best, filename='checkpoint.pth.tar'):
-    
-    file_path = os.path.join(save_path, str(name)+filename)
-    
-    torch.save(state, file_path)
-    if is_best:
-        shutil.copyfile(file_path, os.path.join(save_path, str(name)+'model_best.pth.tar'))
+	
+	file_path = os.path.join(save_path, str(name)+filename)
+	
+	torch.save(state, file_path)
+	if is_best:
+		shutil.copyfile(file_path, os.path.join(save_path, str(name)+'model_best.pth.tar'))
 
 def load_checkpoint(device, save_path, name, filename='checkpoint.pth.tar'):
-    file_path = os.path.join(save_path, str(name)+filename)
-    if os.path.exists(file_path):
-        print("=> loading checkpoint '{}'".format(file_path))
-        return torch.load(file_path, map_location=device)
-    else:
-        print("=> no checkpoint found at '{}'".format(file_path))
-        return None
+	file_path = os.path.join(save_path, str(name)+filename)
+	if os.path.exists(file_path):
+		print("=> loading checkpoint '{}'".format(file_path))
+		return torch.load(file_path, map_location=device)
+	else:
+		print("=> no checkpoint found at '{}'".format(file_path))
+		return None
 
 
 class AverageMeter(object):
-    def __init__(self):
-        self.reset()
+	def __init__(self):
+		self.reset()
 
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
+	def reset(self):
+		self.val = 0
+		self.avg = 0
+		self.sum = 0
+		self.count = 0
 
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
+	def update(self, val, n=1):
+		self.val = val
+		self.sum += val * n
+		self.count += n
+		self.avg = self.sum / self.count
 
 
 def setup_seed(seed):
-    print('Setting up seed: {}'.format(seed))
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    np.random.seed(seed)
-    random.seed(seed)
-    torch.backends.cudnn.deterministic = True
+	print('Setting up seed: {}'.format(seed))
+	torch.manual_seed(seed)
+	torch.cuda.manual_seed_all(seed)
+	np.random.seed(seed)
+	random.seed(seed)
+	torch.backends.cudnn.deterministic = True
 
 
 
 
 def warmup_lr(epoch, step, optimizer, one_epoch_step, args):
-    overall_steps = args.warmup * one_epoch_step
-    current_steps = epoch * one_epoch_step + step
+	overall_steps = args.warmup * one_epoch_step
+	current_steps = epoch * one_epoch_step + step
 
-    lr = args.lr * current_steps / overall_steps
-    lr = min(lr, args.lr)
+	lr = args.lr * current_steps / overall_steps
+	lr = min(lr, args.lr)
 
-    for p in optimizer.param_groups:
-        p["lr"] = lr
+	for p in optimizer.param_groups:
+		p["lr"] = lr
 
 
 def dataset_convert_to_test(dataset, args=None):
-    if args.dataset == "TinyImagenet":
-        test_transform = transforms.Compose([])
-    else:
-        test_transform = transforms.Compose(
-            [
-                transforms.ToTensor(),
-            ]
-        )
-    while hasattr(dataset, "dataset"):
-        dataset = dataset.dataset
-    dataset.transform = test_transform
-    dataset.train = False
+	if args.dataset == "TinyImagenet":
+		test_transform = transforms.Compose([])
+	else:
+		test_transform = transforms.Compose(
+			[
+				transforms.ToTensor(),
+			]
+		)
+	while hasattr(dataset, "dataset"):
+		dataset = dataset.dataset
+	dataset.transform = test_transform
+	dataset.train = False

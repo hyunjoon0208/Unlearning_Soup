5ed9facf9d375374d023d0da94f107ec4e33c6d0
hyunjoon0208 <juatis96@kookmin.ac.kr>
2024-05-23 22:18:47 +0900

    fix error

--- script/pruning.sh
+++ script/pruning.sh
@@ -1,10 +1,10 @@
 ### pruning 
 
 #omp
-python -u pruning.py --data ./data --dataset cifar10 --prune_type rewind_lt --rewind_epoch 8 --save_dir result/pruning/omp --pruning_times 2 --num_workers 8
+python -u /home/20191674/Unlearning_Soup/pruning.py --data ./data --dataset cifar10 --prune_type rewind_lt --rewind_epoch 8 --save_dir result/pruning/omp --pruning_times 2 --num_workers 8
 #imp
-python -u pruning.py --data ./data --dataset cifar10 --prune_type rewind_lt --rewind_epoch 8 --save_dir result/pruning/imp --rate 0.2 --pruning_times 2 --num_workers 8
+python -u /home/20191674/Unlearning_Soup/pruning.py --data ./data --dataset cifar10 --prune_type rewind_lt --rewind_epoch 8 --save_dir result/pruning/imp --rate 0.2 --pruning_times 2 --num_workers 8
 #synFlow
-python -u pruning.py --data ./data --dataset cifar10 --prune_type rewind_lt --rewind_epoch 8 --save_dir result/pruning/synFlow --pruning_times 1 --num_workers 8
+python -u /home/20191674/Unlearning_Soup/pruning.py --data ./data --dataset cifar10 --prune_type rewind_lt --rewind_epoch 8 --save_dir result/pruning/synFlow --pruning_times 1 --num_workers 8
 
 
